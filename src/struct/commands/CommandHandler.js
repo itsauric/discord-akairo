103f869c46076e00d5236d6dab853e731405f3c6
Kaname <inmortaldragonxspace@outlook.com>
2021-04-26 08:14:18 -0600

    Fixed client voice channel not found error.

--- src/struct/commands/CommandHandler.js
+++ src/struct/commands/CommandHandler.js
@@ -658,7 +658,7 @@ class CommandHandler extends AkairoHandler {
                     return true;
                 }
             } else if (message.guild) {
-                const missing = message.member.voice ? message.member.voice.channel.permissionsFor(this.client.user).missing(command.clientPermissions) : message.channel.permissionsFor(this.client.user).missing(command.clientPermissions);
+                const missing = message.member.voice.channelID ? message.member.voice.channel.permissionsFor(this.client.user).missing(command.clientPermissions) : message.channel.permissionsFor(this.client.user).missing(command.clientPermissions);
                 if (missing.length) {
                     this.emit(CommandHandlerEvents.MISSING_PERMISSIONS, message, command, 'client', missing);
                     return true;
